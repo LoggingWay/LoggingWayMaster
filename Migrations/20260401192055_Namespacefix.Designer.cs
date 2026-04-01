354e13585e09e97e9deb2afaf60a0870209fd6ea
Noevain <noevain@gmail.com>
2026-04-01 21:21:30 +0200

    Migrations fixes

--- Migrations/20260401192055_Namespacefix.Designer.cs
+++ Migrations/20260401192055_Namespacefix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LoggingWayMaster.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LoggingWayMaster.Migrations
 {
     [DbContext(typeof(LoggingwayDbContext))]
-    partial class LoggingwayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260401192055_Namespacefix")]
+    partial class Namespacefix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
